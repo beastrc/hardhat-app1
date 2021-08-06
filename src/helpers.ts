94c57c96939859ad15f19c7ff6fd7db7e58698c0
beastrc <makarov0114@outlook.com>
2021-08-06 18:28:24 +0300

    Add Fantom, Fantom testnet, Hoo Smart Chain support

--- src/helpers.ts
+++ src/helpers.ts
@@ -1160,7 +1160,7 @@ Note that in this case, the contract deployment will not behave the same if depl
           '0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103'
         );
         const currentOwner = getAddress(
-          `0x${ownerStorage.substr(-40)}`
+          BigNumber.from(ownerStorage).toHexString()
         );
 
         const oldProxy = proxy.abi.find(
